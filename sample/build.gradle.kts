b1a61fb986b88f4d0fd73ed120be6b9e164f39fb
Leonard <leo@leonard-lemke.com>
2025-03-06 22:33:20 +0100

    bump deps

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -60,6 +60,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("io.github.oneuiproject:icons:1.1.0")
 
-    implementation("androidx.core:core-splashscreen:1.2.0-alpha02")
+    implementation("androidx.core:core-splashscreen:1.2.0-beta01")
     implementation(project(":libpianoview"))
 }
